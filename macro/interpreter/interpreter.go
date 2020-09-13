cf8d0e1cb1ae2925611cf6a2978a40cba1dccc98
dxkite <dxkite@163.com>
2020-09-13 16:53:17 +0800

    调整：测试用例

--- macro/interpreter/interpreter.go
+++ macro/interpreter/interpreter.go
@@ -30,19 +30,19 @@ func (it *interpreter) Eval(node ast.Node, name string, pos token.FilePos) []byt
 	it.Func = map[string]ast.FuncDefineStmt{}
 	it.src = &bytes.Buffer{}
 	it.pos = pos
-	it.eval(node)
+	it.evalStmt(node)
 	return it.src.Bytes()
 }
 
-// 执行AST
-func (it *interpreter) eval(node ast.Node) {
+// 执行宏语句
+func (it *interpreter) evalStmt(node ast.Node) {
 	switch n := node.(type) {
 	case *ast.BlockStmt:
 		for _, sub := range *n {
-			it.eval(sub)
+			it.evalStmt(sub)
 		}
 	case *ast.MacroLitArray:
-		it.src.WriteString(it.evalLitArray(n, false))
+		it.src.WriteString(it.parseLitArray(n, false))
 	case *ast.Ident:
 		it.src.WriteString(it.evalIdent(n))
 	case *ast.ValDefineStmt:
@@ -51,8 +51,33 @@ func (it *interpreter) eval(node ast.Node) {
 }
 
 // 解析表达式
-func (it interpreter) evalExpr(expr ast.Expr) interface{} {
-	return false
+// 返回表达式最后的值(数值/标识符/字符串)
+// 解析宏/宏函数 得到展开的宏
+// 把展开的宏作为表达式解析并返回表达式的值
+func (it interpreter) evalExpr(expr interface{}) interface{} {
+	switch xx := expr.(type) {
+	case uint8, int32, float64, *ast.Ident:
+		return xx
+	case ast.Expr:
+		switch xx := xx.(type) {
+		case *ast.ParenExpr:
+			return it.evalExpr(xx.X)
+		case *ast.UnaryExpr:
+			return it.evalUnaryExpr(xx)
+		case *ast.BinaryExpr:
+			return it.evalBinaryExpr(xx)
+		case *ast.MacroCallExpr:
+			return it.evalCall(xx)
+		}
+		it.errorf(xx.Pos(), "unexpected token")
+	case string:
+		exp, errs := parser.ParseExpr([]byte(xx))
+		if len(errs) > 0 {
+			it.errorf(0, "error expr %s", xx)
+		}
+		return it.evalExpr(exp)
+	}
+	return nil
 }
 
 // 定义值
@@ -61,7 +86,7 @@ func (it *interpreter) evalDefineVal(stmt *ast.ValDefineStmt) {
 	if _, ok := it.Val[n]; ok || it.isInnerDefine(n) {
 		it.errorf(stmt.Pos(), "warning: %s redefined", n)
 	}
-	it.Val[n] = it.evalLitArray(stmt.Body, true)
+	it.Val[n] = it.parseLitArray(stmt.Body, true)
 	f := it.pos.CreatePosition(stmt.From).Line
 	t := it.pos.CreatePosition(stmt.To).Line
 	it.src.WriteString(strings.Repeat("\n", t-f+1))
@@ -79,84 +104,222 @@ func (it interpreter) isInnerDefine(name string) bool {
 }
 
 // 解析列表
-func (it interpreter) evalLitArray(array *ast.MacroLitArray, empty bool) string {
+func (it interpreter) parseLitArray(array *ast.MacroLitArray, empty bool) string {
 	t := ""
 	for _, v := range *array {
-		switch vv := v.(type) {
-		case *ast.Text:
-			if parser.TokenNotIn(vv.Kind, token.BACKSLASH_NEWLINE, token.BLOCK_COMMENT) {
-				t += vv.Text
-			}
-			if vv.Kind == token.BACKSLASH_NEWLINE && !empty {
-				t += "\n"
-			}
-		case *ast.MacroCallExpr:
-			t += it.evalCall(vv)
-		case *ast.Ident:
-			t += it.evalIdent(vv)
-		}
+		t += it.parseLitItem(v, empty)
 	}
 	return t
 }
 
+func (it interpreter) parseLitItem(v ast.Expr, empty bool) string {
+	switch vv := v.(type) {
+	case *ast.Text:
+		if parser.TokenNotIn(vv.Kind, token.BACKSLASH_NEWLINE, token.BLOCK_COMMENT) {
+			return vv.Text
+		} else if vv.Kind == token.BACKSLASH_NEWLINE && !empty {
+			return "\n"
+		}
+	case *ast.MacroCallExpr:
+		return it.evalCall(vv)
+	case *ast.Ident:
+		return it.evalIdent(vv)
+	}
+	return ""
+}
+
+// 执行函数表达式
 func (it interpreter) evalCall(expr *ast.MacroCallExpr) string {
 	return ""
 }
 
+// 解析函数参数
+func (it interpreter) parseCallParamList(list *ast.MacroLitArray) []string {
+	params := []string{}
+	for _, item := range *list {
+		p := ""
+		switch t := item.(type) {
+		case *ast.MacroLitArray:
+			p = it.parseLitArray(t, true)
+		default:
+			p = it.parseLitItem(t, true)
+		}
+		params = append(params, p)
+	}
+	return params
+}
+
 // 二元运算
 func (it interpreter) evalBinaryExpr(expr *ast.BinaryExpr) interface{} {
-	x := it.evalExpr(expr.X)
-	y := it.evalExpr(expr.Y)
-	return evalBinary(x, y, expr.Op)
+	return uint8(0)
+}
+
+// 表达式到整数
+func (it interpreter) intExpr(value interface{}) int32 {
+	switch ex := value.(type) {
+	case *ast.LitExpr:
+		if ex.Kind == token.INT {
+			return it.evalInt(ex)
+		}
+		if ex.Kind == token.CHAR {
+			return int32(charValue(ex.Value))
+		}
+		if ex.Kind == token.FLOAT {
+			return int32(it.evalFloat(ex))
+		}
+	case float64:
+		return int32(ex)
+	case uint8:
+		return int32(ex)
+	case int32:
+		return ex
+	}
+	return 0
+}
+
+// 表达式到浮点数
+func (it interpreter) floatExpr(value interface{}) float64 {
+	switch ex := value.(type) {
+	case *ast.LitExpr:
+		if ex.Kind == token.INT {
+			return it.evalFloat(ex)
+		}
+		if ex.Kind == token.INT {
+			return float64(it.evalInt(ex))
+		}
+		if ex.Kind == token.CHAR {
+			return float64(charValue(ex.Value))
+		}
+	case float64:
+		return ex
+	case uint8:
+		return float64(ex)
+	case int32:
+		return float64(ex)
+	}
+	return 0
+}
+
+// 特殊转义
+var chMap = map[uint8]uint8{
+	'a':  7,
+	'b':  8,
+	'f':  12,
+	'n':  10,
+	'r':  13,
+	't':  9,
+	'v':  11,
+	'\\': 92,
+	'\'': 39,
+	'"':  34,
+}
+
+// 字符串值
+func charValue(ch string) uint8 {
+	v, _ := tryCharValue(ch)
+	return v
+}
+
+func tryCharValue(ch string) (uint8, bool) {
+	l := len(ch)
+	if l <= 2 {
+		return 0, false
+	}
+	if ch[0] != '\'' && ch[l-1] != '\'' {
+		return 0, false
+	}
+	ch = ch[1 : l-1] // ''
+	// x
+	if l == 1 {
+		return ch[0], true
+	} else if ch[0] == '\\' {
+		// \000
+		// \xff
+		i := 1
+		base := 8
+		switch ch[1] {
+		case 'x':
+			i = 2
+			base = 16
+		case 'a', 'b', 'f', 'n', 'r', 't', 'v', '\\', '"', '\'':
+			return chMap[ch[1]], true
+		}
+		x := 0
+		for i < len(ch) {
+			x = x*base + digitVal(rune(ch[i]))
+			i++
+		}
+		return uint8(x), true
+	}
+	return 0, false
+}
+
+func lower(ch rune) rune { return ('a' - 'A') | ch }
+func digitVal(ch rune) int {
+	switch {
+	case '0' <= ch && ch <= '9':
+		return int(ch - '0')
+	case 'a' <= lower(ch) && lower(ch) <= 'f':
+		return int(lower(ch) - 'a' + 10)
+	}
+	return 16
 }
 
 // 一元运算
 func (it interpreter) evalUnaryExpr(expr *ast.UnaryExpr) interface{} {
 	switch expr.Op {
-	case token.LNOT: // !number
-		v := it.evalExpr(expr.X)
-		switch n := v.(type) {
-		case int64:
-			return !(n > 0)
-		case float64:
-			return !(n > 0)
-		default:
-			it.errorf(expr.X.Pos(), "unexpected token %v in LNOT expr", expr.X)
+	case token.NOT: // ~
+		v := it.expectedValue(it.evalExpr(expr))
+		if vv, ok := v.(uint8); ok {
+			return ^vv
 		}
-	case token.NOT: // ~number
-		v := it.evalExpr(expr.X)
-		switch n := v.(type) {
-		case int64:
-			return ^n
-		case float64:
-			return ^int64(n)
-		default:
-			it.errorf(expr.X.Pos(), "unexpected token %v in LNOT expr", expr.X)
+		if vv, ok := v.(int32); ok {
+			return ^vv
 		}
-	case token.SUB: // - number
-		v := it.evalExpr(expr.X)
-		switch n := v.(type) {
-		case int64:
-			return -n
-		case float64:
-			return -n
-		default:
-			it.errorf(expr.X.Pos(), "unexpected token %v in LNOT expr", expr.X)
+	case token.LNOT: // !
+		v := it.expectedValue(it.evalExpr(expr))
+		if vv, ok := v.(uint8); ok {
+			return !(vv > 0)
+		}
+		if vv, ok := v.(int32); ok {
+			return !(vv > 0)
+		}
+		if vv, ok := v.(float64); ok {
+			return !(vv > 0)
+		}
+	case token.SUB: // -
+		v := it.expectedValue(it.evalExpr(expr))
+		if vv, ok := v.(uint8); ok {
+			return -vv
+		}
+		if vv, ok := v.(int32); ok {
+			return -vv
+		}
+		if vv, ok := v.(float64); ok {
+			return -vv
 		}
 	case token.DEFINED: // defined ident
-		id, _ := expr.X.(*ast.Ident)
-		if _, ok := it.Val[id.Name]; ok {
-			return int64(1)
+		ex := it.evalExpr(expr.X)
+		if id, ok := ex.(*ast.Ident); ok {
+			if _, ok := it.Val[id.Name]; ok {
+				return uint8(1)
+			}
+			if _, ok := it.Func[id.Name]; ok {
+				return uint8(1)
+			}
+		} else {
+			it.errorf(expr.X.Pos(), "'defined' is not followed by a ident")
+			return uint8(0)
 		}
-		if _, ok := it.Func[id.Name]; ok {
-			return int64(1)
-		}
-		return int64(0)
 	case token.MACRO: // #ident
-		id, _ := expr.X.(*ast.Ident)
-		return strconv.QuoteToGraphic(it.evalIdent(id))
+		if id, ok := expr.X.(*ast.Ident); ok {
+			return strconv.QuoteToGraphic(it.evalIdent(id))
+		} else {
+			it.errorf(expr.X.Pos(), "'#' is not followed by a macro parameter")
+		}
 	}
-	return ""
+	it.errorf(expr.X.Pos(), "unexpected value %v in unary expr", expr)
+	return uint8(0)
 }
 
 // 解析ID标识符
@@ -170,25 +333,46 @@ func (it interpreter) evalIdent(id *ast.Ident) string {
 	return id.Name
 }
 
-// 解析字面量表达式去运算
-func (it interpreter) evalLitExpr(expr *ast.LitExpr) interface{} {
-	if expr.Kind == token.INT {
-		return it.evalInt(expr)
+// 转换表达式到值
+func (it interpreter) expectedValue(value interface{}) interface{} {
+	switch v := value.(type) {
+	case *ast.LitExpr:
+		if v.Kind == token.FLOAT {
+			return it.evalFloat(v)
+		}
+		if v.Kind == token.INT {
+			return it.evalInt(v)
+		}
+		if v.Kind == token.CHAR {
+			return charValue(v.Value)
+		}
+	case float64, uint8, int32:
+		return v
+	case *ast.Ident:
+		vv := it.evalIdent(v)
+		if v, err := strconv.ParseInt(vv, 0, 32); err == nil {
+			return v
+		}
+		if v, err := strconv.ParseFloat(vv, 64); err == nil {
+			return v
+		}
+		if v, ok := tryCharValue(vv); ok {
+			return v
+		}
+		it.errorf(v.Pos(), "unexpected ident value %v", v)
+	case ast.Expr:
+		it.errorf(v.Pos(), "unexpected value %v", v)
 	}
-	if expr.Kind == token.FLOAT {
-		return it.evalFloat(expr)
-	}
-	it.errorf(expr.Offset, " token %s is not valid in preprocessor expressions", strconv.Quote(expr.Value))
-	return 0
+	return uint8(0)
 }
 
 // 解析数字
-func (it interpreter) evalInt(expr *ast.LitExpr) int64 {
-	v, err := strconv.ParseInt(expr.Value, 0, 64)
+func (it interpreter) evalInt(expr *ast.LitExpr) int32 {
+	v, err := strconv.ParseInt(expr.Value, 0, 32)
 	if err != nil {
 		it.errorf(expr.Offset, "error parse int %s", err.Error())
 	}
-	return v
+	return int32(v)
 }
 
 // 解析数字（浮点数）
--- macro/interpreter/interpreter_test.go
+++ macro/interpreter/interpreter_test.go
@@ -2,9 +2,7 @@ package interpreter
 
 import (
 	"bytes"
-	"dxkite.cn/language/macro/ast"
 	"dxkite.cn/language/macro/parser"
-	"dxkite.cn/language/macro/token"
 	"fmt"
 	"io/ioutil"
 	"os"
@@ -14,115 +12,71 @@ import (
 	"testing"
 )
 
-func Test_interpreter_evalLitExpr(t *testing.T) {
-	it := interpreter{}
-	i := it.evalLitExpr(&ast.LitExpr{
-		Offset: 0,
-		Kind:   token.INT,
-		Value:  "0x123ABCDE",
-	})
-	if i != int64(0x123ABCDE) {
-		t.Error("error parse token.INT 0x123ABCEE")
-	}
-	f := it.evalLitExpr(&ast.LitExpr{
-		Offset: 0,
-		Kind:   token.FLOAT,
-		Value:  "0x123ABCEEp10",
-	})
-	if f != 0x123ABCEEp10 {
-		t.Error("error parse token.FLOAT 0x123ABCEE")
-	}
-}
+//func Test_interpreter_evalLitExpr(t *testing.T) {
+//	it := interpreter{}
+//	i := it.evalLitExpr(&ast.LitExpr{
+//		Offset: 0,
+//		Kind:   token.INT,
+//		Value:  "0x123ABCDE",
+//	})
+//	if i != int64(0x123ABCDE) {
+//		t.Error("error parse token.INT 0x123ABCEE")
+//	}
+//	f := it.evalLitExpr(&ast.LitExpr{
+//		Offset: 0,
+//		Kind:   token.FLOAT,
+//		Value:  "0x123ABCEEp10",
+//	})
+//	if f != 0x123ABCEEp10 {
+//		t.Error("error parse token.FLOAT 0x123ABCEE")
+//	}
+//}
 
-/*
-func TestGenBinaryExpr(t *testing.T) {
-	tf := []token.Token{
-		token.ADD,
-		token.MUL,
-		token.SUB,
-		token.QUO,
-		token.LSS, // <
-		token.GTR, // >
-		token.LEQ, // <=
-		token.GEQ, // >=
-		token.EQL, // ==
-		token.NEQ, // !=
-	}
-	ti := []token.Token{
-		token.REM, // %
-		token.AND, // &
-		token.OR,  // |
-		token.XOR, // ^
-		token.SHL, // <<
-		token.SHR, // >>
-	}
-	tb := []token.Token{
-		token.LAND, // &&
-		token.LOR,  // ||
-	}
-	ftpl := `case %s:
-		if ixo {
-			if iyo {
-				return ix %s iy
-			} else {
-				return float64(ix) %s fy
-			}
-		} else {
-			if iyo {
-				return fx %s float64(iy)
-			} else {
-				return fx %s fy
-			}
-		}
-	`
-	btpl := `case %s:
-		if ixo {
-			if iyo {
-				return (ix > 0) %s ( iy > 0)
-			} else {
-				return (ix > 0) %s  (fy >0 )
-			}
-		} else {
-			if iyo {
-				return (fx> 0)  %s (iy > 0)
-			} else {
-				return (fx> 0)  %s (fy >0 )
-			}
-		}
-	`
-	itpl := `case %s:
-		if ixo {
-			if iyo {
-				return ix %s iy
-			} else {
-				return ix %s int64(fy)
-			}
-		} else {
-			if iyo {
-				return int64(fx) %s iy
-			} else {
-				return int64(fx) %s int64(fy)
-			}
-		}
-	`
-	for _, tok := range tf {
-		tn := "token." + token.Name(tok).String()
-		op := tok.String()
-		fmt.Printf(ftpl, tn, op, op, op, op)
-	}
-	for _, tok := range ti {
-		tn := "token." + token.Name(tok).String()
-		op := tok.String()
-		fmt.Printf(itpl, tn, op, op, op, op)
-	}
-	for _, tok := range tb {
-		tn := "token." + token.Name(tok).String()
-		op := tok.String()
-		fmt.Printf(btpl, tn, op, op, op, op)
-	}
-}
-
-*/
+//
+//func TestGenBinaryExpr(t *testing.T) {
+//	tf := []token.Token{
+//		token.ADD,
+//		token.MUL,
+//		token.SUB,
+//		token.QUO,
+//		token.LSS, // <
+//		token.GTR, // >
+//		token.LEQ, // <=
+//		token.GEQ, // >=
+//		token.EQL, // ==
+//		token.NEQ, // !=
+//	}
+//	//ti := []token.Token{
+//	//	token.REM, // %
+//	//	token.AND, // &
+//	//	token.OR,  // |
+//	//	token.XOR, // ^
+//	//	token.SHL, // <<
+//	//	token.SHR, // >>
+//	//}
+//	//tb := []token.Token{
+//	//	token.LAND, // &&
+//	//	token.LOR,  // ||
+//	//}
+//	ftpl := `case %s:
+//		return xx %s yy
+//	`
+//	for _, tok := range tf {
+//		tn := "token." + token.Name(tok).String()
+//		op := tok.String()
+//		fmt.Printf(ftpl, tn, op)
+//	}
+//	//for _, tok := range ti {
+//	//	tn := "token." + token.Name(tok).String()
+//	//	op := tok.String()
+//	//	fmt.Printf(itpl, tn, op, op, op, op)
+//	//}
+//	//for _, tok := range tb {
+//	//	tn := "token." + token.Name(tok).String()
+//	//	op := tok.String()
+//	//	fmt.Printf(btpl, tn, op, op, op, op)
+//	//}
+//}
 
 func exists(p string) bool {
 	_, err := os.Stat(p)
@@ -151,7 +105,7 @@ func testFile(name, src string, t *testing.T) {
 		t.Error(p.ErrorList())
 	}
 	it := interpreter{}
-	it.Eval(stmts, src, p.FilePos())
+	it.Eval(stmts, name, p.FilePos())
 	pp := path.Join(".", src+".txt")
 	if exists(pp) {
 		txt, err := ioutil.ReadFile(pp)
@@ -159,11 +113,11 @@ func testFile(name, src string, t *testing.T) {
 			t.Error(err)
 		}
 		if bytes.Equal(txt, it.src.Bytes()) == false {
-			t.Fatalf("macro eval error\nwant:\n%s\ngot:\n%s\n",
+			t.Fatalf("macro evalStmt error\nwant:\n%s\ngot:\n%s\n",
 				strconv.QuoteToGraphic(string(txt)), strconv.QuoteToGraphic(it.src.String()))
 		}
 	} else {
-		fmt.Println("write eval file", pp)
+		fmt.Println("write evalStmt file", pp)
 		_ = ioutil.WriteFile(pp, it.src.Bytes(), os.ModePerm)
 	}
 }
@@ -172,7 +126,7 @@ func TestEval(t *testing.T) {
 	if err := filepath.Walk("testdata/", func(p string, info os.FileInfo, err error) error {
 		ext := filepath.Ext(p)
 		name := filepath.Base(p)
-		if ext == ".src" {
+		if ext == ".c" {
 			t.Run(p, func(t *testing.T) {
 				testFile(name, p, t)
 			})
@@ -180,5 +134,44 @@ func TestEval(t *testing.T) {
 		return nil
 	}); err != nil {
 		t.Error(err)
+	}
+}
+
+func Test_charValue(t *testing.T) {
+	tests := []struct {
+		ch   string
+		want uint8
+	}{
+		{
+			"'A'",
+			'A',
+		},
+		{
+			"'\\0'",
+			0,
+		},
+		{
+			"'\\xAB'",
+			0xAB,
+		},
+		{
+			"'\\301'",
+			0301,
+		},
+		{
+			"'\\''",
+			'\'',
+		},
+		{
+			"'\\\"'",
+			'"',
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.ch, func(t *testing.T) {
+			if got := charValue(tt.ch); got != tt.want {
+				t.Errorf("charValue() = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
